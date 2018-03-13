80808730fcc516db94938dfc6580b9a412f0c626
eXtremal-ik7 <eXtremal.ik7@gmail.com>
2018-03-13 13:57:59 +0700

    ui: change coin name to XPM

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -40,9 +40,9 @@ QString BitcoinUnits::longName(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("BTC");
-    case mBTC: return QString("mBTC");
-    case uBTC: return QString::fromUtf8("µBTC (bits)");
+    case BTC: return QString("XPM");
+    case mBTC: return QString("mXPM");
+    case uBTC: return QString::fromUtf8("µXPM (bits)");
     default: return QString("???");
     }
 }
@@ -60,9 +60,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("Bitcoins");
-    case mBTC: return QString("Milli-Bitcoins (1 / 1" THIN_SP_UTF8 "000)");
-    case uBTC: return QString("Micro-Bitcoins (bits) (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
+    case BTC: return QString("Primecoins");
+    case mBTC: return QString("Milli-Primecoins (1 / 1" THIN_SP_UTF8 "000)");
+    case uBTC: return QString("Micro-Primecoins (bits) (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
     default: return QString("???");
     }
 }
