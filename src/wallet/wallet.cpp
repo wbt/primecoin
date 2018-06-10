4031644e3a6ffe050178c351e77f9a76641f8719
eXtremal-ik7 <eXtremal.ik7@gmail.com>
2018-06-11 00:29:43 +0700

    Add hardcoded intermediary nodes; blocks to maturity = COINBASE_MATURITY+200

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -4180,7 +4180,7 @@ int CMerkleTx::GetBlocksToMaturity() const
 {
     if (!IsCoinBase())
         return 0;
-    return std::max(0, (COINBASE_MATURITY+1) - GetDepthInMainChain());
+    return std::max(0, (COINBASE_MATURITY+200) - GetDepthInMainChain());
 }
 
 
