5403717388d3bd5741cc87fcc4a8038e2b3e5523
eXtremal-ik7 <eXtremal.ik7@gmail.com>
2018-06-10 15:57:00 +0700

    Version string update

--- src/clientversion.h
+++ src/clientversion.h
@@ -35,6 +35,12 @@
 #include <string>
 #include <vector>
 
+// primecoin client version - intended for display purpose ONLY
+#define PRIMECOIN_VERSION_MAJOR       0
+#define PRIMECOIN_VERSION_MINOR       1
+#define PRIMECOIN_VERSION_REVISION    5
+#define PRIMECOIN_VERSION_BUILD       0
+
 static const int CLIENT_VERSION =
                            1000000 * CLIENT_VERSION_MAJOR
                          +   10000 * CLIENT_VERSION_MINOR
@@ -44,6 +50,11 @@ static const int CLIENT_VERSION =
 extern const std::string CLIENT_NAME;
 extern const std::string CLIENT_BUILD;
 
+static const int PRIMECOIN_VERSION =
+                           1000000 * PRIMECOIN_VERSION_MAJOR
+                         +   10000 * PRIMECOIN_VERSION_MINOR
+                         +     100 * PRIMECOIN_VERSION_REVISION
+                         +       1 * PRIMECOIN_VERSION_BUILD;
 
 std::string FormatFullVersion();
 std::string FormatSubVersion(const std::string& name, int nClientVersion, const std::vector<std::string>& comments);
