1230865b0742c8a0e1a3c2e5e8e1126bf55deb6f
eXtremal-ik7 <eXtremal.ik7@gmail.com>
2018-06-11 11:43:06 +0700

    MacOS X deploy update

--- src/qt/macnotificationhandler.mm
+++ src/qt/macnotificationhandler.mm
@@ -13,7 +13,7 @@
 - (NSString *)__bundleIdentifier
 {
     if (self == [NSBundle mainBundle]) {
-        return @"org.bitcoinfoundation.Bitcoin-Qt";
+        return @"org.primecoin.Primecoin-Qt";
     } else {
         return [self __bundleIdentifier];
     }
