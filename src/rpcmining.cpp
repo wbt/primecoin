90060bce6231161bcc1256a0b7827d1d6f5afb76
aardvark3 <44991066+aardvark3@users.noreply.github.com>
2021-02-21 21:58:49 +0000

    Return RPC error for submitblock on block validation error (#93)

--- src/rpcmining.cpp
+++ src/rpcmining.cpp
@@ -382,7 +382,7 @@ Value submitblock(const Array& params, bool fHelp)
     CValidationState state;
     bool fAccepted = ProcessBlock(state, NULL, &pblock);
     if (!fAccepted)
-        return "rejected"; // TODO: report validation state
+        throw JSONRPCError(RPC_MISC_ERROR, "rejected"); // TODO: report validation state
 
     return Value::null;
 }
